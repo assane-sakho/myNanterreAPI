d69919bac2668d344a9f006c56b40267909ba20c
assane-sakho <assanesakho.pro@gmail.com>
2020-12-10 12:24:27 +0100

    [ADD] - Relation Club

---
+++ src/Entity/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * UserType
+ *
+ * @ORM\Table(name="user_type")
+ * @ORM\Entity
+ */
+class UserType
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @Groups({"club:read", "club:write"})
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     * @Groups({"club:read", "club:write"})
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+}
